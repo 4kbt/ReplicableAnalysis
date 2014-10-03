c59de87992bdfbf0a42a92cbdf24bbeaca0252e9
charlie <charlie@dicke.nanoradian.com>
2014-10-03 14:04:02 -0700

    More prose, keeps rolling

--- analysis/determineDensity.m
+++ analysis/determineDensity.m
@@ -22,10 +22,10 @@ cylLengthErr = sqrt(cylLengthMeasurementErr^2 + ...
 cylVolumeErr = sqrt(  (pi * cylLength * cylDiameter * cylDiameterErr)^2 + ...
 		      (pi * (cylDiameter/2)^2 * cylLengthErr)^2 );
 
-printSIErr( cylVolume, cylVolumeErr, 2, 0, 'm^{3}', [HOMEDIR '/extracted/cylVolume.tex']);
+printSIErr( cylVolume, cylVolumeErr, 2, 0, 'm$^{3}$', [HOMEDIR '/extracted/cylVolume.tex']);
 
 
 waterDensityErr = sqrt( ( waterMass/cylVolume^2 * cylVolumeErr )^2 + ...
 			( waterMassErr/cylVolume) ^2 );
 
-printSIErr( waterDensity, waterDensityErr, 2, 3, 'g/m^{3}', [HOMEDIR '/extracted/waterDensity.tex']);
+printSIErr( waterDensity, waterDensityErr, 2, 3, 'g/m$^{3}$', [HOMEDIR '/extracted/waterDensity.tex']);
--- globalConfig/experimentalConstants.m
+++ globalConfig/experimentalConstants.m
@@ -1,15 +1,18 @@
 %UNBLIND HERE
 cylDiameter = 1.25*0.0254;
 cylDiameterMeasurementErr = 0.002*0.0254; %expected
+printSIErr( cylDiameter, cylDiameterMeasurementErr, 2, -3, 'm', [HOMEDIR '/extracted/cylDiameter.tex'] );
 
 cylDiameterFabricationErr = 1e-3*0.0254; %determined by dial indicator
+printSI(cylDiameterFabricationErr, 1, -3, 'm', [HOMEDIR '/extracted/cylDiameterFabricationErr.tex']);
 
 %UNBLIND HERE
 cylLength = 2.6 * 0.0254; 
 cylLengthMeasurementErr = 0.002*0.0254; %expected 
+printSIErr( cylLength, cylLengthMeasurementErr, 2, -3, 'm', [HOMEDIR '/extracted/cylLength.tex'] );
 
 cylLengthFabricationErr = sqrt(2)*1.5e-3*0.0254; %determined by dial indicator
-
+printSI(cylLengthFabricationErr, 1, -3, 'm', [HOMEDIR '/extracted/cylLengthFabricationErr.tex']);
 
 scaleToSI = 0.001;
 
